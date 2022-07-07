fb336d3fc7aa77c11745aefcb2c57ee0e5a0088e
harshalkaigaonkar <harshalkaigaonkar31@gmail.com>
2022-07-08 01:39:36 +0530

    Updated API Request

--- src/data/dataFetched.js
+++ src/data/dataFetched.js
@@ -1,5 +1,5 @@
 const dataFetch = async () => {
-  const response = await fetch('https://run.mocky.io/v3/fefcfbeb-5c12-4722-94ad-b8f92caad1ad');
+  const response = await fetch('https://run.mocky.io/v3/4d8db890-5327-4c69-a3ef-b4f5f5225d17');
   const jres = await response.json();
   return jres;
 };
